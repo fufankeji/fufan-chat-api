16098f9a2e65b4b9f2733e49e67f6f684d60aabc
MuYu <snowball980123@gmail.com>
2024-06-17 19:06:08 +0800

    upload basic rag flow

---
+++ playground/test_langchainRag/langchain_rag.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+在LangChain中使用GLM-4 实现基于 Chroma 向量数据库的RAG完整过程。
+"""
+
+# 1. 在控制台先安装 pip install --upgrade langchain langchain-community langchainhub httpx httpx-sse PyJWT langchain-chroma bs4 python-dotenv
+
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.chat_models import ChatZhipuAI
+from zhipuai import ZhipuAI
+import os
+from dotenv import load_dotenv
+
+
+# Step 1. 在“.env”的文件中写入 ZHIPUAI_API_KEY， 并在当前文件中读取zhipu API Keys的信息
+load_dotenv()
+
+
+# Step 2  . 初始化模型, 该行初始化与 智谱 的 GLM - 4  模型进行连接，将其设置为处理和生成响应。
+chat = ChatZhipuAI(
+    model="glm-4",
+    temperature=0.8,
+)
+
+# Step 3 . WebBaseLoader 配置为专门从 Lilian Weng 的博客文章中抓取和加载内容。它仅针对网页的相关部分（例如帖子内容、标题和标头）进行处理。
+"""
+LangChain中关于 文本加载器的集成：
+ - 原生实现的：https://python.langchain.com/v0.2/docs/how_to/#document-loaders
+ - 外部集成的：https://python.langchain.com/v0.2/docs/integrations/document_loaders/
+"""
+
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+
+# Step 4. 使用 RecursiveCharacterTextSplitter 将内容分割成更小的块，这有助于通过将长文本分解为可管理的大小并有一些重叠来保留上下文来管理长文本。
+"""
+LangChain中关于文档切分（Text splitters）：
+- 原生实现的：https://python.langchain.com/v0.2/docs/how_to/#text-splitters
+- 文档链接：https://python.langchain.com/v0.1/docs/modules/data_connection/document_transformers/
+"""
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+
+# splits 是一个列表，其中每个元素也是一个列表，表示一个文档的分割结果
+# for doc_index, doc_splits in enumerate(splits):
+#     print(f"Document {doc_index + 1}:")  # 显示文档编号
+#     for split_index, split_text in enumerate(doc_splits):
+#         print(f"  Split {split_index + 1}: {split_text[:50]}...")  # 打印每个分段的前50个字符
+#     print("\n" + "-"*60 + "\n")  # 在每个文档之间加入分隔线，增加可读性
+
+
+# Step 4. 对每个块，构建Embeddings。 智谱的 GLM Embedding 在线模型：https://open.bigmodel.cn/dev/api#text_embedding
+# embeddings = []
+#
+# from zhipuai import ZhipuAI
+#
+# client = ZhipuAI()
+#
+# for doc_splits in splits:
+#     for split_type, split_content in doc_splits:
+#         if split_type == 'page_content' and split_content.strip():  # 确保处理的是 'page_content' 且内容不为空
+#             try:
+#                 response = client.embeddings.create(
+#                     model="embedding-2",
+#                     input=split_content
+#                 )
+#                 if hasattr(response, 'data'):
+#                     embeddings.append(response.data[0].embedding)
+#
+#                 else:
+#                     print("未能成功获取嵌入向量")
+#             except Exception as e:
+#                 print(f"请求失败，错误信息：{e}")
+#
+# # # 打印嵌入向量
+# for i, embedding in enumerate(embeddings):
+#     print(f"Embedding {i + 1}: {embedding[:3]}...")  # 仅展示前10个值以示例
+
+
+# step 5. Chroma 使用 GLM 4 的 Embedding 模型 提供的嵌入从这些块创建向量存储，从而促进高效检索。
+class EmbeddingGenerator:
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.client = ZhipuAI()
+
+    def embed_documents(self, texts):
+        embeddings = []
+        for text in texts:
+            response = self.client.embeddings.create(model=self.model_name, input=text)
+            if hasattr(response, 'data') and response.data:
+                embeddings.append(response.data[0].embedding)
+            else:
+                # 如果获取嵌入失败，返回一个零向量
+                embeddings.append([0] * 1024)  # 假设嵌入向量维度为 1024
+        return embeddings
+
+
+    def embed_query(self, query):
+        # 使用相同的处理逻辑，只是这次只为单个查询处理
+        response = self.client.embeddings.create(model=self.model_name, input=query)
+        if hasattr(response, 'data') and response.data:
+            return response.data[0].embedding
+        return [0] * 1024  # 如果获取嵌入失败，返回零向量
+
+# GLM 4 官方调用说明：https://open.bigmodel.cn/dev/api#vector
+embedding_generator = EmbeddingGenerator(model_name="embedding-2")
+
+# 文本列表
+texts = [content for document in splits for split_type, content in document if split_type == 'page_content']
+
+
+# Step 6. 创建 Chroma VectorStore， 并存入向量。
+# 源码：https://api.python.langchain.com/en/latest/_modules/langchain_chroma/vectorstores.html#Chroma
+chroma_store = Chroma(
+    collection_name="example_collection",
+    embedding_function=embedding_generator,  # 使用定义的嵌入生成器实例
+    create_collection_if_not_exists=True
+)
+
+
+# 添加文本到 Chroma VectorStore
+IDs = chroma_store.add_texts(texts=texts)
+# print("Added documents with IDs:", IDs)
+
+
+# vectorstore 被转换为一个检索器，能够根据查询获取最相关的文本片段。
+# 自定义函数 format_docs 用于适当地格式化这些片段。
+# RAG 链集成了检索、提示工程（通过 hub.pull ）和初始化语言模型 ( llm ) 来处理查询并生成响应，最后使用 StrOutputParser 。
+
+# 使用 Chroma VectorStore 创建检索器
+# 官方文档：https://python.langchain.com/v0.2/docs/how_to/vectorstore_retriever/
+retriever = chroma_store.as_retriever()
+
+# 这里假设 'hub.pull' 是从某处获取提示的方法
+prompt = hub.pull("rlm/rag-prompt")
+
+# 自定义函数 format_docs 用于适当地格式化这些片段。
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+# vectorstore 被转换为一个检索器，能够根据查询获取最相关的文本片段。
+
+# Step 7. RAG 链集成了检索、提示工程（通过 hub.pull ）和初始化语言模型 ( llm ) 来处理查询并生成响应，最后使用 StrOutputParser 。
+"""
+其过程如下：
+1. 查询输入：字符串“什么是任务分解？”作为输入传递给 rag_chain 。
+2. 上下文检索：链的 retriever 组件链接到矢量存储，激活以从索引的博客内容中查找并获取最相关的文本片段。这些片段是根据与问题的语义相似性与查询最匹配的片段。
+3. 格式化检索的内容：然后， format_docs 函数获取这些检索到的文档，并将它们格式化为单个字符串，每个文档内容由双换行符分隔。此格式化字符串提供了一个连贯的上下文，其中封装了回答查询所需的所有相关信息。
+4. 生成答案：此格式化上下文字符串与查询一起被输入到 glm-4 模型中。该模型使用提供的上下文和查询的细节，根据检索到的信息生成上下文相关且准确的响应。
+5. 输出解析：最后， ChatZhipu 模型生成的响应由 StrOutputParser 进行解析，将模型的输出转换为干净的、用户可读的格式。
+"""
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | chat
+    | StrOutputParser()
+)
+
+
+# Step 8. 进行提问
+"""
+1. 查询处理：该命令接受查询“什么是任务分解？”并将其传递给 retriever 组件。检索器本质上是系统中的搜索功能，设置为在预先索引的数据集中查找信息 - 这里是根据博客内容创建的矢量存储。
+2. 语义搜索：检索器使用向量存储中存储的文本片段的嵌入（向量表示）来执行语义搜索。它将查询的向量表示与存储的片段的向量进行比较，以识别在语义上与查询最相似的片段。
+3. 检索相关文本片段：根据相似度分数，检索器从博客中选择并返回与查询最匹配的文本片段。这些片段包含被认为与回答任务分解问题最相关的信息。
+"""
+
+rag_res = rag_chain.invoke("What is Task Decomposition?")
+print(rag_res)
+
+
+# Step 9. 此命令指示 vectorstore 删除其保存的整个数据集合。这里的集合是指所有文档（文本片段）及其相应的已被索引并存储在向量存储中的向量表示的集合。
+chroma_store.delete_collection()
---
+++ playground/test_langchainRag/langchain_rag_with_SQLAlchemy.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+使用 SQLAlchemy 在 SQLite 数据库中保存聊天历史记录，需要进行一些关键的添加和修改。以下是这些更改的分步说明：
+"""
+
+# Step 1. 首先，导入必要的 SQLAlchemy 模块来设置数据库和 ORM（对象关系映射）。这些模块将允许以 Python 方式与 SQLite 数据库进行交互。
+import bs4
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+from sqlalchemy.exc import SQLAlchemyError
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_models import ChatZhipuAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+from sqlalchemy.exc import SQLAlchemyError
+from zhipuai import ZhipuAI
+import os
+from dotenv import load_dotenv
+
+
+# 在控制台先安装 pip install --upgrade  langchain langchain-community langchainhub httpx httpx-sse PyJWT langchain-chroma bs4 python-dotenv sqlalchemy
+
+# Step 2. 在“.env”的文件中写入 ZHIPUAI_API_KEY， 并在当前文件中读取zhipu API Keys的信息
+load_dotenv()
+
+Base = declarative_base()
+
+# Step 3. 定义可以使用Base类管理的模型类
+class Session(Base):
+    """
+    Session 类表示聊天会话
+    """
+    __tablename__ = "sessions"
+    id = Column(Integer, primary_key=True)
+    session_id = Column(String, unique=True, nullable=False)
+    messages = relationship("Message", back_populates="session")
+
+
+class Message(Base):
+    """
+    Message 类表示会话中的各个消息
+    """
+    __tablename__ = "messages"
+    id = Column(Integer, primary_key=True)
+    session_id = Column(Integer, ForeignKey("sessions.id"), nullable=False)
+    role = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    session = relationship("Session", back_populates="messages")
+
+
+def get_db():
+    """
+    创建一个实用程序函数来管理数据库会话。该函数将确保每个数据库会话正确打开和关闭。
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def save_message(session_id: str, role: str, content: str):
+    """
+    定义一个函数将各个消息保存到数据库中。该函数检查会话是否存在；如果没有，它就会创建一个。然后它将消息保存到相应的会话中。
+    """
+    db = next(get_db())
+    try:
+        # Check if the session already exists
+        session = db.query(Session).filter(Session.session_id == session_id).first()
+        if not session:
+            # Create a new session if it doesn't exist
+            session = Session(session_id=session_id)
+            db.add(session)
+            db.commit()
+            db.refresh(session)
+
+        # Add the message to the session
+        db.add(Message(session_id=session.id, role=role, content=content))
+        db.commit()
+    except SQLAlchemyError:
+        db.rollback()
+    finally:
+        db.close()
+
+def load_session_history(session_id: str) -> BaseChatMessageHistory:
+    """
+    定义一个函数来从数据库加载聊天历史记录。此函数检索与给定会话 ID 关联的所有消息并重建聊天历史记录。
+    """
+    db = next(get_db())
+    chat_history = ChatMessageHistory()
+    try:
+        # Retrieve the session
+        session = db.query(Session).filter(Session.session_id == session_id).first()
+        if session:
+            # Add each message to the chat history
+            for message in session.messages:
+                chat_history.add_message({"role": message.role, "content": message.content})
+    except SQLAlchemyError:
+        pass
+    finally:
+        db.close()
+
+    return chat_history
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    """
+    更新 get_session_history 函数以从数据库检索会话历史记录，而不是仅使用内存存储。
+    """
+    if session_id not in store:
+        store[session_id] = load_session_history(session_id)
+    return store[session_id]
+
+
+def save_all_sessions():
+    """
+    添加退出应用程序之前保存所有会话的功能。此函数迭代内存中的所有会话并将其消息保存到数据库中。
+    增加错误处理以确保程序稳定性。
+    """
+    from langchain_core.messages import HumanMessage
+    from langchain_core.messages import AIMessage
+
+    print(store)
+    for session_id, chat_history in store.items():
+        for message in chat_history.messages:
+            # 检查 message 是否为字典类型
+            if isinstance(message, dict):
+                # 检查字典是否包含必要的键
+                if "role" in message and "content" in message:
+                    save_message(session_id, message["role"], message["content"])
+                else:
+                    print(f"Skipped a message due to missing keys: {message}")
+            # 处理 HumanMessage 和 AIMessage 类型的消息
+            elif isinstance(message, HumanMessage):
+                save_message(session_id, "human", message.content)
+            elif isinstance(message, AIMessage):
+                save_message(session_id, "ai", message.content)
+            else:
+                print(f"Skipped an unsupported message type: {message}")
+
+
+
+def invoke_and_save(session_id, input_text):
+    """
+    修改链式调用函数，同时保存用户问题和AI答案。这确保了每次交互都被记录下来。
+    """
+    # Save the user question with role "human"
+    save_message(session_id, "human", input_text)
+
+    # Get the AI response
+    result = conversational_rag_chain.invoke(
+        {"input": input_text},
+        config={"configurable": {"session_id": session_id}}
+    )["answer"]
+
+    print(f"invoke_and_save:{result}")
+
+    # Save the AI answer with role "ai"
+    save_message(session_id, "ai", result)
+
+    return result
+
+
+class EmbeddingGenerator:
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.client = ZhipuAI()
+
+    def embed_documents(self, texts):
+        embeddings = []
+        for text in texts:
+            response = self.client.embeddings.create(model=self.model_name, input=text)
+            if hasattr(response, 'data') and response.data:
+                embeddings.append(response.data[0].embedding)
+            else:
+                # 如果获取嵌入失败，返回一个零向量
+                embeddings.append([0] * 1024)  # 假设嵌入向量维度为 1024
+        return embeddings
+
+
+    def embed_query(self, query):
+        # 使用相同的处理逻辑，只是这次只为单个查询处理
+        response = self.client.embeddings.create(model=self.model_name, input=query)
+        if hasattr(response, 'data') and response.data:
+            return response.data[0].embedding
+        return [0] * 1024  # 如果获取嵌入失败，返回零向量
+
+
+if __name__ == '__main__':
+
+
+    # Step 1. 定义模型示例
+    chat = ChatZhipuAI(
+        model="glm-4",
+        temperature=0.5,
+    )
+
+    # Step 2. 定义 SQLite 数据库以及用于存储会话和消息的模型。
+    DATABASE_URL = "sqlite:///chat_history.db"
+
+
+    # Step 3. 创建模型类. 官网：https://docs.sqlalchemy.org/en/20/orm/quickstart.html
+    engine = create_engine(DATABASE_URL)
+    Base.metadata.create_all(engine)
+
+    # Step 4. 构建Session 来管理会话。官方Docs：https://docs.sqlalchemy.org/en/20/orm/session_basics.html
+    SessionLocal = sessionmaker(bind=engine)
+
+    # 使用 atexit 模块来注册一个函数 save_all_sessions，这个函数将在Python程序即将正常终止时自动执行。目的是在程序退出前保存所有会话数据，以确保不会因程序突然终止而丢失数据。
+    import atexit
+
+    atexit.register(save_all_sessions)
+
+    ### Construct retriever ###
+    loader = WebBaseLoader(
+        web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(
+                class_=("post-content", "post-title", "post-header")
+            )
+        ),
+    )
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    # 创建嵌入生成器实例
+    embedding_generator = EmbeddingGenerator(model_name="embedding-2")
+
+    # 文本列表
+    texts = [content for document in splits for split_type, content in document if split_type == 'page_content']
+
+    # 创建 Chroma VectorStore
+    chroma_store = Chroma(
+        collection_name="example_collection",
+        embedding_function=embedding_generator,  # 使用定义的嵌入生成器实例
+        create_collection_if_not_exists=True
+    )
+
+    # 添加文本到 Chroma VectorStore
+    IDs = chroma_store.add_texts(texts=texts)
+    # print("Added documents with IDs:", IDs)
+
+    # 使用 Chroma VectorStore 创建检索器
+    retriever = chroma_store.as_retriever()
+
+    ### Contextualize question ###
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        chat, retriever, contextualize_q_prompt
+    )
+
+    ### Answer question ###
+    qa_system_prompt = """You are an assistant for question-answering tasks. \
+    Use the following pieces of retrieved context to answer the question. \
+    If you don't know the answer, just say that you don't know. \
+    Use three sentences maximum and keep the answer concise.\
+
+    {context}"""
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(chat, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+    ### Statefully manage chat history ###
+    store = {}
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+
+    result = invoke_and_save("abc123", "What is Task Decomposition?")
+    print(result)
+
+    # 此命令指示 vectorstore 删除其保存的整个数据集合。这里的集合是指所有文档（文本片段）及其相应的已被索引并存储在向量存储中的向量表示的集合。
+    chroma_store.delete_collection()

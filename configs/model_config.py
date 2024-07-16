4d149fa8807f4db9843313891805080bd4baf4e8
MuYu <snowball980123@gmail.com>
2024-07-16 11:15:31 +0800

    .

--- configs/model_config.py
+++ configs/model_config.py
@@ -4,22 +4,17 @@ MODEL_ROOT_PATH = ""
 
 TEMPERATURE = 0.8
 
-# LLM_MODELS = ["zhipu-api"]
-
-LLM_MODELS = ["chatglm3-6b", "zhipu-api"]
-# LLM_MODELS = ["chatglm3-6b"]
+# 默认启动的模型，如果使用的是glm3-6b，请替换模型名称
+LLM_MODELS = ["glm-4-9b-chat", "zhipu-api"]
 
 RERANKER_MODEL = "bge-reranker-large"
 RERANKER_MAX_LENGTH = 1024
 # 是否启用reranker模型
-USE_RERANKER = True
+USE_RERANKER = False
 
-Agent_MODEL = None
-HISTORY_LEN = 3
 # 知识库匹配向量数量
 VECTOR_SEARCH_TOP_K = 3
 # 知识库匹配的距离阈值，一般取值范围在0-1之间，SCORE越小，距离越小从而相关度越高。
-# 但有用户报告遇到过匹配分值超过1的情况，为了兼容性默认设为1，在WEBUI中调整范围为0-2
 SCORE_THRESHOLD = 1.0
 # 搜索引擎匹配结题数量
 SEARCH_ENGINE_TOP_K = 3
@@ -27,6 +22,9 @@ SEARCH_ENGINE_TOP_K = 3
 MODEL_PATH = {
     # 这里定义 本机服务器上存储的大模型权重存储路径
     "local_model": {
+        # 默认使用glm4-9b-chat
+        "glm-4-9b-chat": "/home/00_rag/model/ZhipuAI/glm-4-9b-chat",
+
         "chatglm3-6b": "/home/00_rag/model/ZhipuAI/chatglm3-6b/",
 
         # 可扩展其他的开源大模型
@@ -36,7 +34,6 @@ MODEL_PATH = {
     # 这里定义 本机服务器上存储的Embedding模型权重存储路径
     "embed_model": {
         "bge-large-zh-v1.5": "/home/00_rag/model/AI-ModelScope/bge-large-zh-v1___5",
-
 
         # 可扩展其他的Embedding模型
     },
@@ -51,7 +48,7 @@ ONLINE_LLM_MODEL = {
 
     # 智谱清言的在线API服务
     "zhipu-api": {
-        "api_key": "",
+        "api_key": "4b3e2d01ed43db2d40bf06ac87cf6fab.QhvSBwQ1DVx9ak7b",
         "version": "glm-4",
         "provider": "ChatGLMWorker",
     },
@@ -68,15 +65,6 @@ ONLINE_LLM_MODEL = {
 
 }
 
-SUPPORT_AGENT_MODEL = [
-    "openai-api",  # GPT4 模型
-    "qwen-api",  # Qwen Max模型
-    "zhipu-api",  # 智谱AI GLM4模型
-    "Qwen",  # 所有Qwen系列本地模型
-    "chatglm3-6b",
-    "internlm2-chat-20b",
-    "Orion-14B-Chat-Plugin",
-]
 
 # 选用的 Embedding 名称
 EMBEDDING_MODEL = "bge-large-zh-v1.5"
@@ -93,7 +81,3 @@ SEARCH_RERANK_TOP_K = 3
 URL = "https://google.serper.dev/search"
 SERPER_API_KEY = ""  # 这里替换为自己实际的Serper API Key
 
-ZILLIZ_URI = "https://in03-053b56bef5db18e.api.gcp-us-west1.zillizcloud.com"
-ZILLIZ_TOKEN = ""
-ZILLIZ_DIM = 1024
-ZILLIZ_COLLECTION = "muyu"

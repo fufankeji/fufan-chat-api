32f63252f102d05bdf2221d1bbfffc98740aea41
MuYu <snowball980123@gmail.com>
2024-07-16 13:46:05 +0800

    .

--- configs/server_config.py
+++ configs/server_config.py
@@ -7,7 +7,7 @@ LLM_DEVICE = "auto"
 HTTPX_DEFAULT_TIMEOUT = 300.0
 
 # 各服务器默认绑定host。如改为"0.0.0.0"需要修改下方所有XX_SERVER的host
-DEFAULT_BIND_HOST = "192.168.110.131" if sys.platform != "win32" else "127.0.0.1"
+DEFAULT_BIND_HOST = "0.0.0.0" if sys.platform != "win32" else "127.0.0.1"
 
 # api.py server
 API_SERVER = {
